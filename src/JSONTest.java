362d86d72dab89cbda8e14443051dc90bfbfef89
Emmy <emmyfarrell18@gmail.com>
2019-11-06 16:32:03 -0600

    Updating GitIgnore for text files generated by the program

--- src/JSONTest.java
+++ src/JSONTest.java
@@ -6,7 +6,7 @@ public class JSONTest
     {
         JSONFilters json = new JSONFilters();
         //noinspection SpellCheckingInspection
-        List<String> jsonList = json.getUrls("fanficBookMarks.json", "links.txt", true);
+        List<String> jsonList = json.getUrls("fanficBookMarks.json", "FirefoxLinks.txt", true);
         System.out.println("List Count: " + jsonList.size());
 
     }
